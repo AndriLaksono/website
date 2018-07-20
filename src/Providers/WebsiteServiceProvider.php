8ec1e87120b365283b8de8c8463ebbe9d13a7132
Andri Laksono <andrilaksono222@gmail.com>
2018-07-20 21:09:14 +0700

    first commit

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AndriLaksono\Website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        // code...
+    }
+
+    public function register()
+    {
+        // code...
+    }
+
+}
